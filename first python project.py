985e41008bd15f630198eace05d1abe480ae3e81
VijaySharanV <117385197+VJsharan@users.noreply.github.com>
2023-08-08 20:13:38 +0530

    Add files via upload

---
+++ first python project.py
@@ -0,0 +1,20 @@
+#username and password generator in python
+import string
+import random
+
+#username
+name=input("Enter first name: ")
+us=name.lower()
+
+yr=input("Enter birth year ")
+d=input("Enter domain: ")
+z1=input("Enter last name ")
+
+#password
+z=random.randint(100000,999999)
+z=str(z)
+z2=z1.capitalize()
+
+print("Your Custom Credentials are..")
+print(f"Username : {us}{yr}@{d}.com")
+print("Password :",z2+z)
