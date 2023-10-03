cd23772f3f09ce3a28b19a817de54461a2edee2f
VijaySharanV <117385197+VJsharan@users.noreply.github.com>
2023-10-03 21:58:29 +0530

    numguess.py

---
+++ numguess.py
@@ -0,0 +1,23 @@
+import random
+num=random.randint(0,9)
+#redo:
+x=int(input("Enter your guess: "))
+#def gus():
+t=1
+print(num)
+while(x!=num):
+ if(x==num):
+   print(x, "is not the correct guess")
+   t+=1
+ elif (x<num):
+   print(x,"is much lesser")
+   x=int(input("Enter your guess: "))
+   t += 1
+ else:
+   print(x,"is much greater")
+   x=int(input("Enter your guess: "))
+   t += 1
+print(f"It took you {t} tries to find the number {num}")
+tp=float(100/t)
+print(f"You were right {tp}% of the time!")
+#vijaysharanv
