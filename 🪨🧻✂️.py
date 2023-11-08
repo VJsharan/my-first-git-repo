1560ecbd66d1f6f2e7422bc8b656ad0decd65603
VijaySharanV <117385197+VJsharan@users.noreply.github.com>
2023-11-08 20:43:44 +0530

    Update 🪨🧻✂️.py
    so I just changed the code overall 
    Earlier it was working using the if else statement and now it works with the help of the loops and the calls. Also I've enclosed the entire thing within a function called as the game and called it accordingly.. Regards VJ

--- 🪨🧻✂️.py
+++ 🪨🧻✂️.py
@@ -1,14 +1,13 @@
 import random
-you,cpu=0,0
-u=input("Enter your choice: ")
-user=u.lower()
-hands=["rock","paper","scissors","scissor",'papers']
-pc=random.choice(hands)
-print(f"Computer chose {pc}")
-if user not in hands:
-    print("Invalid choice bro!!")
-    u = input("Enter your choice again: ")
 def game():
+    you,cpu=0,0
+    u=input("Enter your choice: ")
+    user=u.lower()
+    hands=["rock","paper","scissors","scissor",'papers']
+    pc=random.choice(hands)
+    if user not in hands:
+        print("Invalid choice bro!!")
+        u = input("Enter your choice again: ")
     if user==pc:
         print("Tied!")
     elif user=="rock":
@@ -26,16 +25,20 @@ def game():
             print("Computer Wins!")
             cpu += 1
     elif user=="scissor":
-        if  pc=="paper":
-            print("you win")
-            you += 1
-        else:
-            print("pc wins")
-            cpu+=1
-if you>cpu:
-    print(f"Your score is {you}")
-else:
-    print(f"PC's score is {cpu}")
-y=input("Do you want to play again?").lower()
-if y=="yes":
+            if(pc=="paper"):
+                print("you win")
+                you += 1
+            else:
+                print("pc wins")
+                cpu+=1
+    if you>cpu:
+        return print(f"Your score is {you}")
+    else:
+        return print(f"PC's score is {cpu}")
+game()
+y=input("Do you want to play again?")
+if(y.lower()=="yes"):
     game()
+else:
+    print("Ok program quitted")
+    quit()
